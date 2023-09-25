222673cfca4a24428f6bb6b67b95a6da60fb5dd7
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-09-25 12:04:56 -0500

    se experimento con la librería de JavaScript p5.js (Color, Formas primitivas, Transformación) )

--- sketch.js
+++ sketch.js
@@ -1,7 +1,55 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(150);
+  rectMode(CENTER);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(255, 150, 0);
+  fill(255);
+  noStroke(0);
+  textSize(20);
+  textStyle(700);
+  text(int(mouseX), 50, 50);
+  text(int(mouseY), 50, 100);
+  let step = frameCount % windowWidth + 100;
+  // Equivalent to translate(x, y);
+  applyMatrix(1, 0, 0, 1, 40 + step, -300);
+  fill(mouseX);
+  stroke(255, 150, 0);
+  strokeWeight(mouseY);
+  circle(0, windowHeight / 2 , 300);
+  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
+  circle(600, windowHeight / 2 , 300);
+  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
+  circle(300, windowHeight / 2 , 300);
+
+  applyMatrix(1, 0, 0, 1, windowHeight / 2 - step, 300);
+  fill(255, 150, 0);
+  noStroke(0);
+  text('INICIAR', 275, 125);
+  textSize(128);
+  textStyle(BOLD);
+  fill(mouseX);
+
+  applyMatrix(1, 0, 0, 1, windowHeight / 2 - step, 300);
+  fill(mouseX);
+  stroke(255, 150, 0);
+  strokeWeight(mouseY);
+  circle(0, windowHeight / 2 , 300);
+  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
+  circle(600, windowHeight / 2 , 300);
+  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
+  circle(300, windowHeight / 2 , 300);
+
+  applyMatrix(1, 0, 0, 1, -400 + step, -300);
+  fill(mouseX);
+  stroke(255, 150, 0);
+  strokeWeight(mouseY);
+  circle(0, windowHeight / 2 , 300);
+  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
+  circle(600, windowHeight / 2 , 300);
+  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
+  circle(300, windowHeight / 2 , 300);
+
 }
