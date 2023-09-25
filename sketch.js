07856d643acb9f647da5f65fb2be470ea7c7cd01
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-09-25 16:52:34 -0500

    la pelota aparece y desaparece por los bordes alterando su tamaño, color y la posición en el eje Y

--- sketch.js
+++ sketch.js
@@ -1,55 +1,39 @@
+let posX;
+let posY;
+let diametro;
+let radio;
+let circulo;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(150);
-  rectMode(CENTER);
+  // print(posX);
+  // posX = posX + 25;
+  // print(posX);
+  // posX *= 8;
+  // print(posX);
+
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  diametro = 30;
+  radio = diametro / 2;
+  circulo = color(255, 150, 0);
 }
 
 function draw() {
-  background(255, 150, 0);
-  fill(255);
+  background(255);
+  posX -= 10;
+  if (posX < -radio) {
+    diametro = random(10, 100);
+    radio = diametro / 2;
+    posX = windowWidth + radio;
+    posY = random(windowHeight);
+    circulo = color(255, random(0, 255), 0);
+  }
+
+  fill(circulo);
   noStroke(0);
-  textSize(20);
-  textStyle(700);
-  text(int(mouseX), 50, 50);
-  text(int(mouseY), 50, 100);
-  let step = frameCount % windowWidth + 100;
-  // Equivalent to translate(x, y);
-  applyMatrix(1, 0, 0, 1, 40 + step, -300);
-  fill(mouseX);
-  stroke(255, 150, 0);
-  strokeWeight(mouseY);
-  circle(0, windowHeight / 2 , 300);
-  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
-  circle(600, windowHeight / 2 , 300);
-  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
-  circle(300, windowHeight / 2 , 300);
+  circle(posX, posY, diametro);
 
-  applyMatrix(1, 0, 0, 1, windowHeight / 2 - step, 300);
-  fill(255, 150, 0);
-  noStroke(0);
-  text('INICIAR', 275, 125);
-  textSize(128);
-  textStyle(BOLD);
-  fill(mouseX);
-
-  applyMatrix(1, 0, 0, 1, windowHeight / 2 - step, 300);
-  fill(mouseX);
-  stroke(255, 150, 0);
-  strokeWeight(mouseY);
-  circle(0, windowHeight / 2 , 300);
-  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
-  circle(600, windowHeight / 2 , 300);
-  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
-  circle(300, windowHeight / 2 , 300);
-
-  applyMatrix(1, 0, 0, 1, -400 + step, -300);
-  fill(mouseX);
-  stroke(255, 150, 0);
-  strokeWeight(mouseY);
-  circle(0, windowHeight / 2 , 300);
-  quad(150, windowHeight / 2 - 300, 0, windowHeight / 2, 150, windowHeight / 2 + 300, 300, windowHeight / 2);
-  circle(600, windowHeight / 2 , 300);
-  quad(450, windowHeight / 2 - 300, 300, windowHeight / 2, 450, windowHeight / 2 + 300, 600, windowHeight / 2);
-  circle(300, windowHeight / 2 , 300);
-
+  print(posX);
+  print(color);
 }
